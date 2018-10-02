a3f7819b977b1ecc74bb90a9a5430c7ab0f5a6e6
evilC <evilc@evilc.com>
2018-10-02 11:59:33 +0100

    Add AxesToAxesTrim

---
+++ AxesToAxesTrim/AxesToAxesTrim.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HidWizards.UCR.Core.Attributes;
+using HidWizards.UCR.Core.Models;
+using HidWizards.UCR.Core.Models.Binding;
+using HidWizards.UCR.Core.Utilities;
+using HidWizards.UCR.Core.Utilities.AxisHelpers;
+
+namespace AxesToAxesTrim
+{
+    [Plugin("Axes to Axes (Trim)")]
+    [PluginInput(DeviceBindingCategory.Range, "X Axis")]
+    [PluginInput(DeviceBindingCategory.Range, "Y Axis")]
+    [PluginInput(DeviceBindingCategory.Momentary, "Trim")]
+    [PluginInput(DeviceBindingCategory.Momentary, "Reset Trim")]
+    [PluginOutput(DeviceBindingCategory.Range, "X Axis")]
+    [PluginOutput(DeviceBindingCategory.Range, "Y Axis")]
+    public class AxesToAxesTrim : Plugin
+    {
+        private readonly CircularDeadZoneHelper _circularDeadZoneHelper = new CircularDeadZoneHelper();
+        private readonly DeadZoneHelper _deadZoneHelper = new DeadZoneHelper();
+        private readonly SensitivityHelper _sensitivityHelper = new SensitivityHelper();
+        private double _linearSenstitivityScaleFactor;
+        private long _trimX;
+        private long _trimY;
+        private bool _trimValueTaken;
+
+        [PluginGui("Invert X", ColumnOrder = 0)]
+        public bool InvertX { get; set; }
+
+        [PluginGui("Invert Y", ColumnOrder = 1)]
+        public bool InvertY { get; set; }
+
+        [PluginGui("Sensitivity", ColumnOrder = 2)]
+        public int Sensitivity { get; set; }
+
+        [PluginGui("Linear", RowOrder = 0, ColumnOrder = 2)]
+        public bool Linear { get; set; }
+
+        [PluginGui("Dead zone", RowOrder = 1, ColumnOrder = 0)]
+        public int DeadZone { get; set; }
+
+        [PluginGui("Circular", RowOrder = 1, ColumnOrder = 2)]
+        public bool CircularDz { get; set; }
+
+
+        public AxesToAxesTrim()
+        {
+            DeadZone = 0;
+            Sensitivity = 100;
+        }
+
+        public override void InitializeCacheValues()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _deadZoneHelper.Percentage = DeadZone;
+            _circularDeadZoneHelper.Percentage = DeadZone;
+            _sensitivityHelper.Percentage = Sensitivity;
+            _linearSenstitivityScaleFactor = ((double)Sensitivity / 100);
+        }
+
+        public override void Update(params long[] values)
+        {
+            if (values[3] == 1)
+            {
+                // Reset held
+                _trimX = 0;
+                _trimY = 0;
+            }
+            else if (values[2] == 1)
+            {
+                // Trim held
+                if (!_trimValueTaken)
+                {
+                    // First iteration through, store new trim value and set _trimValueTaken flag
+                    _trimX += values[0];
+                    _trimY += values[1];
+                    _trimValueTaken = true;
+                }
+            }
+            else
+            {
+                var outputValues = new long[] { values[0], values[1] };
+                if (values[2] == 0) _trimValueTaken = false;    // ToDo: This is clumsy, fix once we can get notification of *which* input changed state
+                if (DeadZone != 0)
+                {
+                    if (CircularDz)
+                    {
+                        outputValues = _circularDeadZoneHelper.ApplyRangeDeadZone(outputValues);
+                    }
+                    else
+                    {
+                        outputValues[0] = _deadZoneHelper.ApplyRangeDeadZone(outputValues[0]);
+                        outputValues[1] = _deadZoneHelper.ApplyRangeDeadZone(outputValues[1]);
+                    }
+
+                }
+                if (Sensitivity != 100)
+                {
+                    if (Linear)
+                    {
+                        outputValues[0] = (long)(outputValues[0] * _linearSenstitivityScaleFactor);
+                        outputValues[1] = (long)(outputValues[1] * _linearSenstitivityScaleFactor);
+                    }
+                    else
+                    {
+                        outputValues[0] = _sensitivityHelper.ApplyRangeSensitivity(outputValues[0]);
+                        outputValues[1] = _sensitivityHelper.ApplyRangeSensitivity(outputValues[1]);
+                    }
+                }
+
+                // Apply trim
+                outputValues[0] += _trimX;
+                outputValues[1] += _trimY;
+
+                outputValues[0] = Functions.ClampAxisRange(outputValues[0]);
+                outputValues[1] = Functions.ClampAxisRange(outputValues[1]);
+
+                if (InvertX) outputValues[0] = Functions.Invert(outputValues[0]);
+                if (InvertY) outputValues[1] = Functions.Invert(outputValues[1]);
+
+                WriteOutput(0, outputValues[0]);
+                WriteOutput(1, outputValues[1]);
+            }
+        }
+    }
+}
