8538c0b55b364b312533ed4058eb049968a77a19
Clive Galway <evilc@evilc.com>
2018-10-29 20:21:05 +0000

    Add EventToButton, fix references

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 ### Removed
 ### Fixed
 
+## [0.0.2] - 2018-10-29
+### Added
+- Added EventToButton plugin
+
 ## [0.0.1] - 2018-10-02
 ### Added
 - Moved AxisToAxisCumulative, AxisToDelta, ButtonToButtonsLongPress and DeltaToAxis plugins from UCR repo to this repo
---
+++ EventToButton/EventToButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using HidWizards.UCR.Core.Attributes;
+using HidWizards.UCR.Core.Models;
+using HidWizards.UCR.Core.Models.Binding;
+
+namespace EventToButton
+{
+    [Plugin("Event to Button")]
+    [PluginInput(DeviceBindingCategory.Event, "Button")]
+    [PluginOutput(DeviceBindingCategory.Momentary, "Button")]
+    public class EventToButton : Plugin
+    {
+
+        [PluginGui("Hold Time", ColumnOrder = 0, RowOrder = 0)]
+        public int HoldTime { get; set; }
+
+        public EventToButton()
+        {
+            HoldTime = 50;
+        }
+
+        public override void Update(params long[] values)
+        {
+            WriteOutput(0, 1);
+            ThreadPool.QueueUserWorkItem(cb => Release());
+        }
+
+        private void Release()
+        {
+            Thread.Sleep(HoldTime);
+            WriteOutput(0, 0);
+        }
+    }
+}
